6d351468968af2e5869bdc3d4c80800d54f74928
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-07-04 02:01:16 +0200

    first work on the algorithm

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,5 +1,65 @@
+//var cols = 1920 / 20;
+//var rows = 980 / 20;
+var cols = 5;
+var rows = 5;
+var grid = new Array(cols);
+var w, h;
+
+var openSet = [];
+var closedSet = [];
+
+var start;
+var end;
+
 function setup() {
-    createCanvas(window.innerHeight, window.innerHeight);
+    //createCanvas(1920, 980);
+    createCanvas(400, 400);
+
+    w = width / cols;
+    h = height / rows;
+
+    for (var i = 0; i < cols; i++) {
+        grid[i] = new Array(rows);
+    }
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Spot(i, j);
+        }
+    }
+
+    console.log(grid);
+    start = grid[0][0];
+    end = grid[cols - 1][rows - 1];
+
+    openSet.push(start);
 }
 
-function draw() {}
+function draw() {
+    if (openSet.length > 0) {
+        var winner = 0;
+        for (var i = 0; i < openSet.length; i++) {
+            if (openSet[i].f < openSet[winner].f) {
+                winner = i;
+            }
+        }
+    } else {
+        // no solution
+    }
+
+    background(0);
+    
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].show(color(255));
+        }
+    }
+
+    for (var i = 0; i < closedSet.length; i++) {
+        closedSet[i].show(color(255, 0, 0));
+    }
+
+    for (var i = 0; i < openSet.length; i++) {
+        openSet[i].show(color(0, 255, 0));
+    }
+}
--- javascript/spot.js
+++ javascript/spot.js
@@ -1,1 +1,13 @@
+function Spot(i, j) {
+    this.i = i;
+    this.j = j;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
 
+    this.show = function (col) {
+        fill(col);
+        noStroke();
+        rect(this.i * w, this.j * h, w - 1, h - 1);
+    };
+}
